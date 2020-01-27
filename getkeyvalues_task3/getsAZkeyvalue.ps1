05517b6917a10693620f5d00aad6b92c18310a82
coolweare <59975816+coolweare@users.noreply.github.com>
2020-01-27 01:24:58 -0500

    Add files via upload

---
+++ getkeyvalues_task3/getsAZkeyvalue.ps1
@@ -0,0 +1,24 @@
+﻿#>
+
+	[CmdletBinding(SupportsShouldProcess=$true)]
+	Param(
+		[Parameter(Mandatory=$true)]
+        [string]$VaultName,
+		[Parameter(Mandatory=$true)]
+        [string]$SecretName,
+		[bool]$PlainText = $true
+    )
+	
+	Write-Verbose -Message $PSCmdlet.MyInvocation.MyCommand
+    Write-Verbose -Message "Get $SecretName from $VaultName"
+
+	if( $PlainText -eq $true ){
+		return (Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName).SecretValueText
+	}
+	else
+	{
+		return (Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName).SecretValue
+	}
+
+Export-ModuleMember Invoke-GetAzureSecretConfigFunction
+Register-SitecoreInstallExtension -Command Invoke-GetAzureSecretConfigFunction -As GetSecret -Type ConfigFunction
